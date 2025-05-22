6a5bd1400bf7048b0745b2e7599b01e326f4ff49
lagirebahan <cj.lagirebahan@gmail.com>
2025-05-22 18:49:11 +0700

    second commit

--- src/sclassdev/sclassdev/settings.py
+++ src/sclassdev/sclassdev/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$9kq2=oga6r8=!ph%#osuhpn@hcnujf@_p27ihgtsj+)ugg#0d'
+SECRET_KEY = 'django-insecure-81)5j*03jfc3k(0gz5cgb@!g7ex5559a(zl64!67ch03ag)9ld'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
